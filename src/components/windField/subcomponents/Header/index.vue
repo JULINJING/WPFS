9bdc7647f1fa7b86f82750ffae6fac30fc3afad2
JULINJING <julinjinghzt@gmail.com>
2023-07-05 17:57:24 +0800

    更新Mars3D大屏动画

--- src/components/windField/subcomponents/Header/index.vue
+++ src/components/windField/subcomponents/Header/index.vue
@@ -1,19 +1,37 @@
 <template>
-    <div class="layout">
-        <div class="layout-header">{{ appName }}</div>
+    <div class="header">
+        <div class="header-title">{{ appName }}</div>
     </div>
 </template>
-<script setup lang="ts">
-const appName = '风场风机可视化平台'
+<script lang="js">
+import $ from 'jquery'
+export default {
+    data() {
+        return{
+            appName:'风场风机可视化平台'
+        }
+    },
+    methods: {
+        initTitle() {
+            setTimeout(function () {
+                $(".header-title").css("display","block")
+                $(".header-title").addClass("animated bounceIn")
+            }, 2000)
+        }
+    },
+    mounted() {
+        this.initTitle()
+    }
+}
 </script>
 <style lang="scss" scoped>
-.layout {
+.header {
     width: 100vw;
     // height: 100vh;
     height: 1px;
     background-color: #000;
 
-    .layout-header {
+    .header-title {
         position: absolute;
         top: 0;
         z-index: 999;
@@ -22,13 +40,14 @@ const appName = '风场风机可视化平台'
         justify-content: center;
         width: 100vw;
         height: 64px;
-        padding-bottom: 10px;
+        padding-top: 10px;
         font-size: 24px;
         font-weight: bold;
         color: #fff;
         background: url('@/assets/images/largeTitle.png');
         background-repeat: no-repeat;
         background-size: 100% 64px;
+        display: none;
     }
 }
 </style>
