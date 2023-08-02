100deadcef4ecd248124c8a691221b5570ec7b94
JULINJING <julinjinghzt@gmail.com>
2023-08-02 21:53:02 +0800

    修改windland

--- src/components/windLand/WindLand.vue
+++ src/components/windLand/WindLand.vue
@@ -19,6 +19,7 @@ import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js'
 
 import NavTop from '../baseComponents/NavTop'
 
@@ -29,13 +30,7 @@ export default {
     },
     data() {
         return {
-            // container: null,
-            // scene: null,
-            // renderer: null,
-            camera: null,
-            // controls: null,
-            // ambient: null,
-            // sunLight: null,
+            camera: null
         };
     },
     methods: {
@@ -44,42 +39,35 @@ export default {
             this.$router.push('/home')
         },
         turbineThree() {
-            /////////////////////////////////////////////////////////////////////////
-            //// DRACO LOADER TO LOAD DRACO COMPRESSED MODELS FROM BLENDER
+            // 从Blender向Draco加载器加载Draco压缩模型
             const dracoLoader = new DRACOLoader()
             const loader = new GLTFLoader()
             dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
             dracoLoader.setDecoderConfig({ type: 'js' })
             loader.setDRACOLoader(dracoLoader)
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// DIV CONTAINER CREATION TO HOLD THREEJS EXPERIENCE
+            // 创造DIV容器容纳three.js
             const container = document.createElement('div')
-            // document.body.appendChild(container)
             this.$refs.windland.appendChild(container)
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// SCENE CREATION
+            // 创建场景
             const scene = new THREE.Scene()
             scene.background = new THREE.Color('#c8f0f9')
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// RENDERER CONFIG
-            const renderer = new THREE.WebGLRenderer({ antialias: true }) // turn on antialias
-            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)) //set pixel ratio
-            renderer.setSize(window.innerWidth, window.innerHeight) // make it full screen
-            renderer.outputEncoding = THREE.sRGBEncoding // set color encoding
-            container.appendChild(renderer.domElement) // add the renderer to html div
+            // renderer配置
+            const renderer = new THREE.WebGLRenderer({ antialias: true }) // 打开抗锯齿
+            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)) // 设置像素比
+            renderer.setSize(window.innerWidth, window.innerHeight) // 全屏
+            renderer.outputEncoding = THREE.sRGBEncoding // 设置颜色编码
+            container.appendChild(renderer.domElement) // 加入renderer
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// CAMERAS CONFIG
+            // 相机配置
             this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100)
             this.camera.position.set(34, 16, -20)
-            // this.camera.lookAt(new THREE.Vector3(0, 0, 0));  // Look at the origin
+            // this.camera.lookAt(new THREE.Vector3(0, 0, 0));  // 回到起始视角
             scene.add(this.camera)
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// MAKE EXPERIENCE FULL SCREEN
+            // 保持全屏
             window.addEventListener('resize', () => {
                 const width = window.innerWidth
                 const height = window.innerHeight
@@ -90,12 +78,10 @@ export default {
                 renderer.setPixelRatio(2)
             })
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// CREATE ORBIT CONTROLS
+            // 创建轨道控制
             const controls = new OrbitControls(this.camera, renderer.domElement)
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// SCENE LIGHTS
+            // 场景光
             const ambient = new THREE.AmbientLight(0xa0a0fc, 0.82)
             scene.add(ambient)
 
@@ -103,35 +89,32 @@ export default {
             sunLight.position.set(-69, 44, 14)
             scene.add(sunLight)
 
-            /////////////////////////////////////////////////////////////////////////
-            ///// LOADING GLB/GLTF MODEL FROM BLENDER
+            // 加载模型
             loader.load('models/gltf/starter-scene.glb', function (gltf) {
-                gltf.scene.rotation.x = THREE.Math.degToRad(90); // Rotate the model by 90 degrees
+                gltf.scene.rotation.x = THREE.Math.degToRad(90); // 旋转90度
                 scene.add(gltf.scene)
             })
 
-            /////////////////////////////////////////////////////////////////////////
-            //// INTRO CAMERA ANIMATION USING TWEEN
+            // 使用渐变介绍相机动画
             var introAnimation = ()=> {
-                controls.enabled = false //disable orbit controls to animate the camera
+                controls.enabled = false // 禁用轨道控制以使相机动画化
 
-                new TWEEN.Tween(this.camera.position.set(16, 50, 10)).to({ // from camera position
-                    x: 26, //desired x position to go
-                    y: 4, //desired y position to go
-                    z: 35 //desired z position to go
-                }, 6500) // time take to animate
-                .delay(1000).easing(TWEEN.Easing.Quartic.InOut).start() // define delay, easing
-                .onComplete(function () { //on finish animation
-                    controls.enabled = true //enable orbit controls
-                    setOrbitControlsLimits() //enable controls limits
-                    TWEEN.remove(this) // remove the animation from memory
+                new TWEEN.Tween(this.camera.position.set(16, 50, 10)).to({ // 从相机位置开始
+                    x: 26, // 想要的x位置
+                    y: 4, // 想要的y位置
+                    z: 35 // 想要的z位置
+                }, 6500) // 动画时间
+                .delay(1000).easing(TWEEN.Easing.Quartic.InOut).start() // 定义延迟、缓和
+                .onComplete(function () { // 完成动画
+                    controls.enabled = true // 启用轨道控制
+                    setOrbitControlsLimits() // 启用控制限制
+                    TWEEN.remove(this) // 从内存中删除动画
                 })
             }
 
-            introAnimation() // call intro animation on start
+            introAnimation() // 动画开始
 
-            /////////////////////////////////////////////////////////////////////////
-            //// DEFINE ORBIT CONTROLS LIMITS
+            // 定义轨道控制限制
             function setOrbitControlsLimits() {
                 controls.enableDamping = true
                 controls.dampingFactor = 0.04
@@ -142,127 +125,57 @@ export default {
                 controls.maxPolarAngle = Math.PI / 2.5
             }
 
-            /////////////////////////////////////////////////////////////////////////
-            //// RENDER LOOP FUNCTION
-            var rendeLoop = ()=> {
+            // 循环渲染函数
+            var renderLoop = ()=> {
 
-                TWEEN.update() // update animations
+                TWEEN.update() // 更新动画
 
-                controls.update() // update orbit controls
+                controls.update() // 更新轨道控制
 
-                renderer.render(scene, this.camera) // render the scene using the camera
+                renderer.render(scene, this.camera) // 使用相机渲染场景
 
-                requestAnimationFrame(rendeLoop) //loop the render function
+                requestAnimationFrame(renderLoop) // 循环渲染函数
 
             }
 
-            rendeLoop() //start rendering
+            renderLoop() // 开始渲染
+
+            // 开启GUI调试
+            const gui = new GUI()
+
+            // 创建GUI参数
+            var params = {color: sunLight.color.getHex(), color2: ambient.color.getHex(), color3: scene.background.getHex()}
+
+            // 创建GUI回调函数
+            const update = function () {
+                var colorObj = new THREE.Color( params.color )
+                var colorObj2 = new THREE.Color( params.color2 )
+                var colorObj3 = new THREE.Color( params.color3 )
+                sunLight.color.set(colorObj)
+                ambient.color.set(colorObj2)
+                scene.background.set(colorObj3)
+            }
+
+            // GUI 配置
+            gui.add(sunLight, 'intensity').min(0).max(10).step(0.0001).name('Dir intensity')
+            gui.add(sunLight.position, 'x').min(-100).max(100).step(0.00001).name('Dir X pos')
+            gui.add(sunLight.position, 'y').min(0).max(100).step(0.00001).name('Dir Y pos')
+            gui.add(sunLight.position, 'z').min(-100).max(100).step(0.00001).name('Dir Z pos')
+            gui.addColor(params,'color').name('Dir color').onChange(update)
+            gui.addColor(params,'color2').name('Amb color').onChange(update)
+            gui.add(ambient, 'intensity').min(0).max(10).step(0.001).name('Amb intensity')
+            gui.addColor(params,'color3').name('BG color').onChange(update)
+
+            // 鼠标移动获得相机位置
+            document.addEventListener('mousemove', (event) => {
+                event.preventDefault()
+
+                console.log(this.camera.position)
+
+            }, false)
         },
-        // // 初始化three配置
-        // initThree() {
-        //     // 创造DIV容器容纳three.js
-        //     var container = document.createElement('div')
-        //     this.$refs.windland.appendChild(container)
-        //     // 创建场景
-        //     var scene = new THREE.Scene();
-        //     scene.background = new THREE.Color('#c8f0f9')
-        //     // renderer配置
-        //     var renderer = new THREE.WebGLRenderer({ antialias: true });// 打开抗锯齿
-        //     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));// 设置像素比
-        //     renderer.setSize(window.innerWidth, window.innerHeight);// 全屏
-        //     renderer.outputEncoding = THREE.sRGBEncoding;// 设置颜色编码
-        //     container.appendChild(renderer.domElement);// 加入renderer
-        //     // 相机配置
-        //     var camera = new THREE.PerspectiveCamera(
-        //         35,
-        //         window.innerWidth / window.innerHeight,
-        //         1,
-        //         100
-        //     );
-        //     camera.position.set(34, 16, -20);
-        //     scene.add(camera);
-
-        //     // 创建轨道控制
-        //     var controls = new OrbitControls(camera, renderer.domElement)
-
-        //     // 场景光
-        //     var ambient = new THREE.AmbientLight(0xa0a0fc, 0.82)
-        //     scene.add(ambient)
-        //     var sunLight = new THREE.DirectionalLight(0xe8c37b, 1.96)
-        //     sunLight.position.set(-69,44,14)
-        //     scene.add(sunLight)
-
-        //     this.container = container;
-        //     this.scene = scene;
-        //     this.renderer = renderer;
-        //     this.camera = camera;
-        //     this.controls = controls;
-        //     this.ambient = ambient;
-        //     this.sunLight = sunLight;
-        // },
-        // // 加载模型
-        // loadModel() {
-        //     var loader = new GLTFLoader();
-        //     var dracoLoader = new DRACOLoader();
-        //     dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
-        //     dracoLoader.setDecoderConfig({ type: 'js' });
-        //     loader.setDRACOLoader(dracoLoader);
-
-        //     loader.load('models/gltf/starter-scene.glb', (gltf) => {
-        //         this.scene.add(gltf.scene);
-        //     });
-        // },
-        // // 使用渐变介绍相机动画
-        // introAnimation() {
-        //     this.controls.enabled = false;//禁用轨道控制以使相机动画化
-        //     new TWEEN.Tween(this.camera.position.set(26, 4, -35)).to({// 从相机位置开始
-        //         x: 16,// 想要的x位置
-        //         y: 50,// 想要的y位置
-        //         z: -0.1,// 想要的z位置
-        //     }, 6500)// 动画时间
-        //     .delay(1000)
-        //     .easing(TWEEN.Easing.Quartic.InOut)
-        //     .start()// 定义延迟、缓和
-        //     .onComplete(() => {// 完成动画
-        //     this.controls.enabled = true;// 启用轨道控制
-        //     this.setOrbitControlsLimits();// 启用控制限制
-        //     TWEEN.remove(this);// 从内存中删除动画
-        //     });
-        // },
-        // //确定轨道控制限制
-        // setOrbitControlsLimits() {
-        //     this.controls.enableDamping = true;
-        //     this.controls.dampingFactor = 0.04;
-        //     this.controls.minDistance = 35;
-        //     this.controls.maxDistance = 60;
-        //     this.controls.enableRotate = true;
-        //     this.controls.enableZoom = true;
-        //     this.controls.maxPolarAngle = Math.PI / 2.5;
-        // },
-        // renderLoop() {
-        //     TWEEN.update();// 更新动画
-        //     this.controls.update();// 更新轨道控制
-        //     this.renderer.render(this.scene, this.camera);// 使用相机渲染场景
-        //     requestAnimationFrame(this.renderLoop); // 循环渲染函数
-        // },
-        // fullScreen() {
-        //     window.addEventListener('resize', () => {
-        //         const width = window.innerWidth;
-        //         const height = window.innerHeight;
-        //         this.camera.aspect = width / height;
-        //         this.camera.updateProjectionMatrix();
-
-        //         this.renderer.setSize(width, height);
-        //         this.renderer.setPixelRatio(2);
-        //     });
-        // },
     },
     mounted() {
-        // this.initThree();
-        // this.loadModel();
-        // this.introAnimation();
-        // this.renderLoop();
-        // this.fullScreen();
         this.turbineThree()
     }
 }
